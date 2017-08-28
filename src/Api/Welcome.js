f8dbbe2c014bd73752073825b8cfadda50aec7e9
Augusto Claro <augustoclaro1@hotmail.com>
2017-08-28 13:35:37 -0300

    fixing welcome.js path to html file

--- src/Api/Welcome.js
+++ src/Api/Welcome.js
@@ -6,9 +6,11 @@ const authentication = require('../shared/Authentication');
 const result = require('../shared/RequestResult');
 const tokenManager = require('../shared/TokenManager');
 const fs = require('fs');
+const path = require('fs');
 
 app.get('/welcome', function(req, res){
-    fs.readFile('../../html/emailverification.html', function (err, data){
+    console.log(path.join(__dirname, '../../html/emailverification.html'));
+    fs.readFile(path.join(__dirname, '../../html/emailverification.html'), function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length': data.length});
         res.write(data);
         res.end();
